6ae367c29bf5bcfe752636c455eb741b65b0fc76
Mohammad Ataei <80059702+mammadataei@users.noreply.github.com>
2022-12-07 08:50:38 +0330

    feat: add client app for graphiql (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  //
+  plugins: [react()],
+  build: {
+    outDir: './dist/client',
+  },
+  publicDir: 'src/client/public',
 })
