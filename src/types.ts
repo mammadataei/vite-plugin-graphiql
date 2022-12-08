71e16d42cdc9cc635045e283a695794e28943d16
Mohammad Ataei <mammadataei@gmail.com>
2022-12-08 20:19:32 +0330

    docs: update README.md

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,13 @@
 export interface GraphiQLPluginOptions {
+  /**
+   * The GraphiQL client configuration.
+   */
   client: ClientOptions
 }
 
 export interface ClientOptions {
+  /**
+   * Url for http(s) requests to the GraphQL server.
+   */
   url: string
 }
